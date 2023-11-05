3579d6036518f06c328a7923f3a0f2a5442388f2
Ahmet Taha Arslan <ahmettahaarslan1@gmail.com>
2023-11-05 22:52:07 +0300

    bugs

--- App.js
+++ App.js
@@ -20,26 +20,7 @@ import { LogLevel, OneSignal } from 'react-native-onesignal';
 
 const  App = () => {
 
-/*  
-  // Remove this method to stop OneSignal Debugging
-//OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 
-// OneSignal Initialization
-OneSignal.initialize("0ca869e3-567c-4c62-bd56-7b6b0e465829");
-
-
-// requestPermission will show the native iOS or Android notification permission prompt.
-// We recommend removing the following code and instead using an In-App Message to prompt for notification permission
-OneSignal.Notifications.requestPermission(true);
-
-
-
-// Method for listening for notification clicks
-OneSignal.Notifications.addEventListener('click', (event) => {
-  console.log('OneSignal: notification clicked:', event);
-});
-
-*/
 
   useEffect(()=> {
     SplashScreen.hide()
